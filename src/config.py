d5caf1b533220df4cc9437d1529884d8acee0491
shellynj <mb884@njit.edu>
2019-12-19 05:46:10 -0500

    Task: Updated config.py and database.conf files for database configuration

--- src/config.py
+++ src/config.py
@@ -1,9 +1,31 @@
 import os
 
-user = os.environ['postgres']
-password = os.environ['Shelly_NJIT_2019']
-host = os.environ['localhost']
-database = os.environ['POSTGRES_DB']
-port = os.environ['5432']
+# Python program to explain os.environ object
 
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+# importing os module
+import os
+import pprint
+
+# Get the list of user's
+# environment variables
+## env_var = os.environ
+
+# Print the list of user's
+# environment variables
+## print("User's Environment variable:")
+## pprint.pprint(dict(env_var), width=1)
+
+
+
+
+#DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{"user"}:{"password"}@{"localhost"}:{5432}/{"POSTGRES_DB"}'
+#DATABASE_CONNECTION_URI = f'postgresql+psycopg2://postgres:password@postgres:5432/example'
+DATABASE_CONNECTION_URI = f'postgresql+psycopg2://postgres:password@localhost:5432/example'
+
+
+
+print("DATABASE_CONNECTION_URI")
+print(DATABASE_CONNECTION_URI)
+
+# set FLASK_APP=app.py
+# C:\Users\User\AppData\Local\Programs\Python\Python38-32
